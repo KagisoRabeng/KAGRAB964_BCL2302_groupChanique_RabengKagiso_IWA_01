9fbcda4a1ed9d938fd279d6d4fd561a8b940e770
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-03-29 10:14:59 +0200

    tally app: javascript

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA01/script.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA01/script.js
@@ -1,3 +1,34 @@
-const number = document.querySelector('.counter__value')
-const substract = document.querySelector
-const add = document.querySelector
+const MAX_NUMBER = 10
+const MIN_NUMBER = -5
+const STEP_AMOUNT = 1
+
+const number = document.querySelector('[data-key="number"]')
+const substract = document.querySelector ('[data-key="substract"]')
+const add = document.querySelector ('[data-key="add"]')
+
+const substractHandler = () => {
+    const newvalue = parseInt(number.value) - STEP_AMOUNT
+    number.value = newvalue
+    if (add.disabled === true) {
+        add.disabled = false
+    }
+
+    if (newvalue <= MIN_NUMBER) {
+        substract.disabled = true
+    } else {
+        substract.disabled = false
+    }
+}
+
+const addHandler = () => {
+    const newvalue = parseInt(number.value)  + STEP_AMOUNT
+    number.value = newvalue
+    if (substract.disabled === true) {
+        substract.disabled = false
+    }
+    if (newvalue >= MAX_NUMBER) {
+        add.disabled = true
+    }
+}
+substract.addEventListener('click', substractHandler)
+add.addEventListener('click', addHandler)
