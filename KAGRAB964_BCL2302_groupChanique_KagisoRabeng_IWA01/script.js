4f91f78118e7056096aac7cbae77a4a7774b66d9
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-03-29 10:50:09 +0200

    added opacity to the buttons

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA01/script.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA01/script.js
@@ -3,10 +3,10 @@ const MIN_NUMBER = -5
 const STEP_AMOUNT = 1
 
 const number = document.querySelector('[data-key="number"]')
-const substract = document.querySelector ('[data-key="substract"]')
+const subtract = document.querySelector ('[data-key="subtract"]')
 const add = document.querySelector ('[data-key="add"]')
 
-const substractHandler = () => {
+const subtractHandler = () => {
     const newvalue = parseInt(number.value) - STEP_AMOUNT
     number.value = newvalue
     if (add.disabled === true) {
@@ -14,21 +14,21 @@ const substractHandler = () => {
     }
 
     if (newvalue <= MIN_NUMBER) {
-        substract.disabled = true
+        subtract.disabled = true
     } else {
-        substract.disabled = false
+        subtract.disabled = false
     }
 }
 
 const addHandler = () => {
     const newvalue = parseInt(number.value)  + STEP_AMOUNT
     number.value = newvalue
-    if (substract.disabled === true) {
-        substract.disabled = false
+    if (subtract.disabled === true) {
+        subtract.disabled = false
     }
     if (newvalue >= MAX_NUMBER) {
         add.disabled = true
     }
 }
-substract.addEventListener('click', substractHandler)
+subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
