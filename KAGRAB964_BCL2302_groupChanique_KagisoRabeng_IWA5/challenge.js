a612cbe8103b8718b0662e7d1d8a792679fa1429
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-04 10:29:22 +0200

    debugging challenge

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA5/challenge.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA5/challenge.js
@@ -1,36 +1,51 @@
 const FREE_WARNING = 'Free shipping only applies to single customer orders'
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-const NONE_SELECTED = '0'
-let shoes = 300 * 1
-let toys = 100 * 5
-let shirts = 150 * 'NONE_SELECTED'
-let batteries = 35 * 2
-let pens = 5 * 'NONE_SELECTED' 
+const NONE_SELECTED = '0' 
 let location = 'RSA'
 let customers = '1'
-let currency = 'R'
+let currency = 'R' || '$' 
 let shipping = null
 
-if (location) { shipping === 400 && currency === 'R'
 
-let location = 'NAM'
 
-if (location) {
+if (location) { shipping === 400 && currency === 'R' }
+
+if (location = 'NAM') {
 	shipping = 600
 } 
-else 
+else {
 (shipping = 800)
 }
 
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * NONE_SELECTED
+let batteries = 35 * 2
+let pens = 5 * NONE_SELECTED
 
-if (shoes + batteries + pens + shirts > 1000) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA) {
-		    shipping = 0 || calcShipping
-		}
-	}
+if (shoes + batteries + pens + shirts + toys > 1000) {
+	shipping = 0
+} 
+else {
+	shipping = shipping + 400
 }
 
-if (shipping = 0 && customers !== 1) { console.log(FREE_WARNING) }
+	if (location = 'NAM' && customers < 2) {
+		shipping = null
+		currency = '$'
+		}
+		else {
+			shipping = 600
+		}
+			if (location = 'RSA') {
+				currency = 'R'
+				shipping = 0
+		}
+		
 
-location === 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+if (shipping = 0 && customers !== 1) { console.log(FREE_WARNING) }
+location = 'NK'
+if (location) {BANNED_WARNING}
+
+console.log('price', currency, shoes + batteries + pens + shirts + toys + shipping)
