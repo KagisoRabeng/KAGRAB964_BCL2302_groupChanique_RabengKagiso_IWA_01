51b23012dfd1ac3f5d21ff2c255ba64a74c9d7e5
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-05 15:04:19 +0200

    debugging challenges

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA5/challenge.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA5/challenge.js
@@ -3,19 +3,8 @@ const BANNED_WARNING = 'Unfortunately we do not ship to your country of residenc
 const NONE_SELECTED = '0' 
 let location = 'RSA'
 let customers = '1'
-let currency = 'R' || '$' 
+let currency = null 
 let shipping = null
-
-
-
-if (location) { shipping === 400 && currency === 'R' }
-
-if (location = 'NAM') {
-	shipping = 600
-} 
-else {
-(shipping = 800)
-}
 
 let shoes = 300 * 1
 let toys = 100 * 5
@@ -23,29 +12,26 @@ let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2
 let pens = 5 * NONE_SELECTED
 
-if (shoes + batteries + pens + shirts + toys > 1000) {
-	shipping = 0
-} 
-else {
-	shipping = shipping + 400
-}
+if (location === 'RSA') {
+		shipping = 400
+		currency = 'R'
+	} 
+	else {
+		shipping = location === 'NAM'? 600 : 800
+		currency = '$'
+	}
 
 	if (location = 'NAM' && customers < 2) {
 		shipping = null
-		currency = '$'
+		let currency = '$'
 		}
 		else {
 			shipping = 600
 		}
-			if (location = 'RSA') {
-				currency = 'R'
-				shipping = 0
-		}
-		
+			
+		if (shipping = 0 && customers !== 1) { console.log(FREE_WARNING) }
+		location = 'NK'
 
-
-if (shipping = 0 && customers !== 1) { console.log(FREE_WARNING) }
-location = 'NK'
-if (location) {BANNED_WARNING}
+		if (location) {console.log(BANNED_WARNING)}
 
 console.log('price', currency, shoes + batteries + pens + shirts + toys + shipping)
--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA6/challenge 2/scripts.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA6/challenge 2/scripts.js
@@ -8,10 +8,15 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if ( hourOfDay == 0 && minuteOfDay == 0) {
-    const taxAsDecimal = parseFloat(tax) / '100'
-    const startingAfterTax = salary - (salary * taxAsDecimal)
-	const balance = startingAfterTax - transport - food - rent
-console.log(balance.toFixed(2))
 
+if ( hourOfDay === 00 && minuteOfDay === 00) {
+    const taxAsDecimal = parseFloat(tax) / 100
+    const startingAfterTax = salary - (salary * taxAsDecimal)
+	let balance = startingAfterTax - transport - food - rent
+
+    console.log(balance.toFixed(2))
 }
+if(hourOfDay !== 00 && minuteOfDay !== 00) {
+ console.log(undefined)
+}
+console.log(balance.toFixed(2))
