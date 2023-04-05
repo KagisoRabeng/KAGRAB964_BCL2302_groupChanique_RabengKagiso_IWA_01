bb191aa18cb833a52ebd6bd56a430ff60b597683
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-05 08:59:52 +0200

    added the second method of debugging

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA6/challenge 1/script.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA6/challenge 1/script.js
@@ -4,7 +4,20 @@ const secondaryPhone = '0219131568'
 // Only change below this line
 
 const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number !== secondaryPhone 
+const secondaryValid = parseInt(secondaryPhone) == secondaryPhone 
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+
+
+// const primaryPhone = 'O748105141'
+// const secondaryPhone = '0219131568'
+
+// // Only change below this line
+
+// const primaryValid = typeof number == primaryPhone 
+// const secondaryValid = typeof number !== secondaryPhone 
+
+// console.log('Primary phone is valid numerical string:', primaryValid)
+// console.log('Secondary phone is valid numerical string:', secondaryValid )
