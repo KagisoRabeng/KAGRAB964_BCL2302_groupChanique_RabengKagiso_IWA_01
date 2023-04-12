a2e6ba7c776cb03cd4241d305a8ab9fb6a484198
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-12 09:23:37 +0200

    debugged challenge

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA7/challenge 2/c-7-2.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA7/challenge 2/c-7-2.js
@@ -1,4 +1,9 @@
- const nickname= "Timmy";
-const firstname = "Timothy";
+ const nickname= false;
+ const firstname = "Timothy";
 
-console.log(`Good Morning, ${nickname}!`)
+// console.log(`Good Morning, ${nickname || firstname}`)
+
+const names = nickname + firstname
+if (names) {
+    console.log(`Good Morning, ${nickname || firstname}!`)
+}
