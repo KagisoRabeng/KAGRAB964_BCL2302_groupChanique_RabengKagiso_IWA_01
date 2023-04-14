176c2b348a64d68aa60f39756888ac9768e43412
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-14 13:14:43 +0200

    debugging challenge

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA11/iwa11.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA11/iwa11.js
@@ -1,33 +1,33 @@
 
 const order1 = document.querySelector('[data-key="order1"]');
-const order1biscuits = order1.querySelector('.biscuits .count');
-const order1donuts = order1.querySelector("donuts .count");
-const order1pancakes = order1.querySelector("pancakes .count");
-const order1status = order1.querySelector(".status dd");
+const biscuits1 = order1.querySelector('.biscuits .count');
+const donuts1 = order1.querySelector('.donuts .count');
+const pancakes1 = order1.querySelector('.pancakes .count');
+const status1 = order1.querySelector('.status dd');
 
 const order2 = document.querySelector('[data-key="order2"]');
-const order2biscuits = order2.querySelector(".biscuits .count");
-const order2donuts = order2.querySelector(".donuts .count");
-const order2pancakes = order2.querySelector(".pancakes .count");
-const order2status = order2.querySelector(".status dd");
+const biscuits2 = order2.querySelector('.biscuits .count');
+const donuts2 = order2.querySelector('.donuts .count');
+const pancakes2 = order2.querySelector('.pancakes .count');
+const status2 = order2.querySelector('.status dd');
 
 const order3 = document.querySelector('[data-key="order3"]');
-const order3biscuits = order3.querySelector(".biscuits .count");
-const order3donuts = order3.querySelector(".donuts .count");
-const order3pancakes = order3.querySelector(".pancakes .count");
-const order3status = order3.querySelector(".status dd");
+const biscuits3 = order3.querySelector('.biscuits .count');
+const donuts3 = order3.querySelector('.donuts .count');
+const pancakes3 = order3.querySelector('.pancakes .count');
+const status3 = order3.querySelector('.status dd');
 
 biscuits1.innerText = order1.dataset.biscuits
 donuts1.innerText = order1.dataset.donuts 
 pancakes1.innerText = order1.dataset.pancakes
-status1.innerText = order1.status = order1.status ? Delivered : Pending
+status1.innerText = order1.status ? Delivered : Pending
 
 biscuits2.innerText = order2.dataset.biscuits
 donuts2.innerText = order2.dataset.donuts 
 pancakes2.innerText = order2.dataset.pancakes
 status2.innerText = order2.status = order2.status ? Delivered : Pending
 
-biscuits3.innerText = order3.dataset.biscuits
+biscuits3.innerText = biscuits3.dataset.biscuits
 donuts3.innerText = order3.dataset.donuts
 pancakes3.innerText = order3.dataset.pancakes
 status3.innerText = order3.status = order3.status ? Delivered : Pending 
