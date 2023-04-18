c254edad2bc41aac98ddb163a1e90dda7caa72b0
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-18 11:25:56 +0200

    debugged challenge

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA13/iwa13.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA13/iwa13.js
@@ -5,24 +5,53 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const logCalc = () => { 
+  const isString = typeof calculated === 'string' 
+  const calculatedAsNumber = isString ? parseInt(calculated) : calculated
+  calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
+// function logCalc(){
+//   isString = typeof calculated === 'numerical-string' 
+//   calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+//   calculated = calculatedAsNumber + 1 
+
+// }
+
+const calcUser = () => {
+  logCalc()
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
+// function calcUser(){
+//   if(calculated < 2){
+//     user = 'John';
+
+//     state = 'requesting';
+
+
+//   }else if(calculated > 3){
+
+//     state = 'idle';
+
+//   }
+// }
+
+const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
 }
+
+// function checkUser(){
+//   if(user && state === 'requesting'){
+
+//     console.log(`User: ${user} (${calculated})`)
+    
+//   }
+// }
 
 // Only allowed to change code above
 
