bd642e41ec82dae54b3594ce3ddbfdbcbdb6f488
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-18 16:22:10 +0200

    debugging challeng 14

---
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA14/iwa14.js
@@ -0,0 +1,31 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = () => {
+    `Hello, ${firstName} (${age}). I love ${hobby}!`
+}
+
+function hobby (parameter) {
+  console.log(parameter);
+}
+
+hobby(logTwice);
+
+// firstName = 'John'
+// age = 35;
+// hobby = 'Coding';
+
+// function hobby () {
+//  console(parameter)
+//  console(parameter)
+// }
+
+// const logTwice = () => {
+//    console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+// }
+
+// logTwice()
+// logTwice()
+
+
