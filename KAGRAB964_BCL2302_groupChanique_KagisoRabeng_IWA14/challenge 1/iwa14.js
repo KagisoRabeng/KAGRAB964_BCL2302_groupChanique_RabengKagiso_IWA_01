7ebb0e74eef49ff8efb81518326f24e3f571b319
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-19 10:44:14 +0200

    debugging challenge 14

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA14/challenge 1/iwa14.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA14/challenge 1/iwa14.js
@@ -3,14 +3,14 @@ age = 35;
 hobby = 'Coding';
 
 const logTwice = () => {
-    `Hello, ${firstName} (${age}). I love ${hobby}!`
+   (`Hello, ${firstName} (${age}). I love ${hobby}!`)
 }
 
 function hobby (parameter) {
-  console.log(parameter);
+  logTwice(parameter);
 }
 
-hobby(logTwice);
+
 
 // firstName = 'John'
 // age = 35;
