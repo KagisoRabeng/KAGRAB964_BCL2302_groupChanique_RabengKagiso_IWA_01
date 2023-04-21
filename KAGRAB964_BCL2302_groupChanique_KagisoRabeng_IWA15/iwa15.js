ec01bcaf344ff371a1662135c8fc8a27f101c61e
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-04-21 11:08:31 +0200

    debugged challenge 15

--- KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA15/iwa15.js
+++ KAGRAB964_BCL2302_groupChanique_KagisoRabeng_IWA15/iwa15.js
@@ -8,22 +8,26 @@ const data = {
 
 // Only edit below
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+const first = data.lists[0][1];
+const second = data.lists[1][1];
+const third = data.lists[2][1];
+
+// const lastOfFirst = first[data.lists.length [-1]];
+// const lastOfSecond = second[second.length -1];
+// const lastOfThird = third[third.length -1]
 
 const result = []
 
 const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
+	if (first[first.length -1] > second[second.length -1] && first[first.length -1] > third[third.length -1]) {
+		return first.pop()
 	}
 
-	if (third[-1] < 1) {
-		return second
+	if (second[second.length -1] > third[third.length -1] ) {
+		return second.pop() 
 	}
 	
-	return third
+	return third.pop()
 }
 
 // Only edit above
